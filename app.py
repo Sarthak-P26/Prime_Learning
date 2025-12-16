33e5f385064598b2ed504dc5aa851196c7314ea0
Sarthak Pawar <sarthakjob2604@gmail.com>
2025-12-16 22:38:11 +0530

    done

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 print("Welcome To Check Your Number Is Prime Or Not")
+play = True
+while play:
+    
 
-# Take an input from a user.
-number = int(input("Enter a number: "))
+    # Take an input from a user.
+    number = int(input("Enter a number: "))
 
-if number <= 1:
-    print("Enter the number greater than 1")
-elif number == 2: 
-    print("2 is a prime number...")
+    if number <= 1:
+        print("Enter the number greater than 1")
+    elif number == 2: 
+        print("2 is a prime number...")
 
 
-for serial_no in range(2, number -1):
-    if number % serial_no == 0:
-        print("It's not a prime number")
-    else:
+    for serial_no in range(2, number):
+        if number % serial_no == 0:
+            value = False
+            break
+        
+        else:
+            value = True
+
+
+    if value == True:
         print("It's a prime number")
+    else:
+        print("It's not a prime number")
 
-
+    user = input("You want to test another number: (yes/no)").lower()
+    if user == 'yes':
+        continue
+    else:
+        play = False
