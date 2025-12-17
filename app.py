98feb0a531829addef1fa71dee0cf4f8cf6ec78b
Sarthak Pawar <sarthakjob2604@gmail.com>
2025-12-17 23:40:58 +0530

    changes were done new function created

--- app.py
+++ app.py
@@ -1,33 +1,46 @@
-print("Welcome To Check Your Number Is Prime Or Not")
-play = True
-while play:
+def take_input():
+    try:
+        number = int(input("Enter the number: "))
+    except ValueError:
+        print("Enter the valid value")
+    return number
+
+
+def set_output(number):
+    if number <= 1:
+        is_prime = False
+    elif number == 2:
+        is_prime = True
+    return is_prime
+
+
+def unknown_output(number):
+    for item in range(2, number):
+        if number % item == 0:
+            isprime = False
+            break
+        else:
+            isprime = True
+    return isprime 
+
+
+def result_showing(number, isprime):
+    if isprime == True:
+        print(f"{number} is a prime number")
+    else:
+        print(f"{number} is not a prime number")
+
+
+print("Check Prime Or Not")
+
+
+
+# play = True
+# while play:
     
 
-    # Take an input from a user.
-    number = int(input("Enter a number: "))
-
-    if number <= 1:
-        print("Enter the number greater than 1")
-    elif number == 2: 
-        print("2 is a prime number...")
-
-
-    for serial_no in range(2, number):
-        if number % serial_no == 0:
-            value = False
-            break
-        
-        else:
-            value = True
-
-
-    if value == True:
-        print("It's a prime number")
-    else:
-        print("It's not a prime number")
-
-    user = input("You want to test another number: (yes/no)").lower()
-    if user == 'yes':
-        continue
-    else:
-        play = False
+#     user = input("You want to test another number: (yes/no)").lower()
+#     if user == 'yes':
+#         continue
+#     else:
+#         play = False
